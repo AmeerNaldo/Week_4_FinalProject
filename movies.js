28f8fcc4b0f95852544e7f4bf443bfe580ee3e81
Ameer <ameerabdrabo7@gmail.com>
2024-08-13 20:12:54 -0500

    final commit

--- movies.js
+++ movies.js
@@ -1,15 +1,25 @@
-const accessKey = "e7fd5705"
+const accessKey = "e7fd5705";
 const movieListEl = document.querySelector(".movie-list");
+const searchResultsEl = document.querySelector(".search__results--for");
 const loading = document.querySelector(".modal__overlay--loading");
 
 async function renderMovies(userSearch) {
-  loading.classList.add("modal__overlay--visible");
+  movieListEl.innerHTML = 
+    `<div class="modal__overlay modal__overlay--loading">
+      <i class="fas fa-spinner"></i>
+    </div>`
+  ;
 
   const movies = await fetch(
-    `http://www.omdbapi.com/?apikey=e7fd5705&s=${userSearch}`
+    `https://www.omdbapi.com/?apikey=e7fd5705&s=${userSearch}`
   );
   const movieSearch = await movies.json();
   console.log(movieSearch);
+  await new Promise(r => setTimeout(r, 500));
+
+  searchResultsEl.innerHTML = 
+  `<div>Search Results for "${userSearch}"</div>`;
+
   movieListEl.innerHTML = movieSearch.Search.map((x) => movieHTML(x)).join("");
 }
 
@@ -17,12 +27,6 @@ function moviePosts(Title) {
   localStorage.setItem("Title", Title);
   window.location.href = `${window.location.origin}/movies.html`;
 }
-
-// function search(event) {
-//   event.preventDefault();
-//   const userSearch = document.getElementById("movie-name").value;
-//   renderMovies(userSearch);  
-// }
 
 function movieHTML(movie) {
   return `<div class="movie-box">
